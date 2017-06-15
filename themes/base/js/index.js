1b2df7befa63ddb96b6db26122dddf96623efdd7
Lucas Frey <lucas.frey@heyday.co.nz>
2017-06-15 21:26:39 +1200

    IF Build

--- themes/base/js/index.js
+++ themes/base/js/index.js
@@ -2,29 +2,4 @@
 
 window.addEventListener('DOMContentLoaded', function() {
 
-	// Lazy load images
-	window.onload = function() {
-		var placeholder = document.querySelectorAll('.placeholder');
-
-		placeholder.forEach(function(image, i) {
-			var small = image.querySelector('.img-small')
-
-			// 1: load small image and show it
-			var img = new Image();
-			img.src = small.src;
-			img.onload = function () {
-				small.classList.add('loaded');
-			};
-
-			// 2: load large image
-			var imgLarge = new Image();
-			imgLarge.src = image.dataset.large;
-			imgLarge.onload = function () {
-				imgLarge.classList.add('loaded');
-				imgLarge.classList.add('column__image');
-			};
-			image.appendChild(imgLarge);
-		});
-
-	}
 });
