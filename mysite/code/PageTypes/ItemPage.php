fe3035d7e872cab1e4116cef47c4fa4b7f7dc4e1
Lucas Frey <lucas.frey@heyday.co.nz>
2017-07-04 21:36:59 +1200

    Update editor field for the french translation

--- mysite/code/PageTypes/ItemPage.php
+++ mysite/code/PageTypes/ItemPage.php
@@ -26,7 +26,7 @@ class ItemPage extends Page
         $fields = parent::getCMSFields();
 
         $fields->addFieldToTab('Root.Main', new TextField('Title', 'Title'));
-        $fields->addFieldToTab('Root.French', new TextField('ContentFR', 'ContentFR'));
+        $fields->addFieldToTab('Root.French', new HtmlEditorField('ContentFR', 'ContentFR'));
         $fields->addFieldToTab('Root.Main', new DateField('Created', 'Created'));
         $fields->addFieldToTab('Root.Main', $uploader = UploadField::create('Photo'));
 
