f6071013320b82ce937c34507b996149db746abc
Lucas Frey <lucas.frey@heyday.co.nz>
2017-07-10 23:25:23 +1200

    Update sorting filters

--- mysite/code/PageTypes/HomePage.php
+++ mysite/code/PageTypes/HomePage.php
@@ -7,10 +7,5 @@ class HomePage extends Page
 
 class HomePage_Controller extends Page_Controller
 {
-    /**
-     * Returns 10 items for the homepage
-     */
-    public function getItems() {
-        return ItemPage::get()->limit(10);
-    }
+
 }
